c005a2d4afa5b9fe54cbf6110f4705b82a957b50
NwaguChima <nwaguchima48@gmail.com>
2022-08-07 23:04:00 +0100

    setup logger middleware

--- middleware/logger.js
+++ middleware/logger.js
@@ -21,3 +21,12 @@ const logEvents = async (message, logFileName) => {
     console.log(error);
   }
 };
+
+const logger = (req, res, next) => {
+  logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, "reqLog.log");
+  console.log(`${req.method} ${req.path}`);
+
+  next();
+};
+
+module.exports = { logger, logEvents };
