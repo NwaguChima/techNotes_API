f3b6031e8be0740d205bb99188e6519b3c4de7a7
NwaguChima <nwaguchima48@gmail.com>
2022-08-07 23:01:13 +0100

    completed logger func

--- middleware/logger.js
+++ middleware/logger.js
@@ -7,4 +7,17 @@ const path = require("path");
 const logEvents = async (message, logFileName) => {
   const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
   const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
+
+  try {
+    if (!fs.existsSync(path.join(__dirname, "..", "logs"))) {
+      await fsPromises.mkdir(path.join(__dirname, "..", "logs"));
+    }
+
+    await fsPromises.appendFile(
+      path.join(__dirname, "..", "logs", logFileName),
+      logItem
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
