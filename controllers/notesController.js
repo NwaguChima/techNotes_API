d2942fdbe3e554d183675a53992a8392d3f47f17
NwaguChima <nwaguchima48@gmail.com>
2022-08-15 03:54:22 +0100

    creat new notes method setup

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -21,3 +21,28 @@ const getAllNotes = asyncHandler(async (req, res) => {
 
   res.json(notesWithUser);
 });
+
+// @desc    Create new note
+// @route   POST /notes
+// @access  Private
+const createNewNote = asyncHandler(async (req, res) => {
+  const { user, title, text } = req.body;
+
+  if (!user || !title || !text) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const duplicate = await Note.findOne({ title }).lean().exec();
+
+  if (duplicate) {
+    return res.status(409).json({ message: "Note title already exists" });
+  }
+
+  const note = await Note.create({ user, title, text });
+
+  if (note) {
+    return res.status(201).json({ message: `New note ${title} created` });
+  } else {
+    return res.status(400).json({ message: "Invalid note data recieved" });
+  }
+});
