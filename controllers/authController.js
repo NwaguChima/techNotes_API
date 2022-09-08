27ef2713fb2e118cb8ae5b06b6cca1d285651f8b
NwaguChima <nwaguchima48@gmail.com>
2022-09-09 00:17:11 +0100

    completed login controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,31 @@ const bycrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
 
-const login = asyncHandler(async (req, res, next) => {});
+const login = asyncHandler(async (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).send({
+      message: "All fields are required",
+    });
+  }
+
+  const foundUser = await User.findOne({ username }).exec();
+
+  if (!foundUser || !foundUser.active) {
+    return res.status(401).send({
+      message: "Unauthorized",
+    });
+  }
+
+  const match = await bycrypt.compare(password, foundUser.password);
+
+  if (!match) {
+    return res.status(401).send({
+      message: "Unauthorized",
+    });
+  }
+});
 
 const refresh = asyncHandler(async (req, res, next) => {});
 
