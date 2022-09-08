467e2cdd35b900a96385b12380209852f72d48aa
NwaguChima <nwaguchima48@gmail.com>
2022-09-09 00:10:02 +0100

    setup the controllers

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,3 +2,9 @@ const User = require("../models/User");
 const bycrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
+
+const login = asyncHandler(async (req, res, next) => {});
+
+const refresh = asyncHandler(async (req, res, next) => {});
+
+const logoout = asyncHandler(async (req, res, next) => {});
