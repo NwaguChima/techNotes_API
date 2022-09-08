a089921654d1bd5da23ad8488c37c3d058269441
NwaguChima <nwaguchima48@gmail.com>
2022-09-09 00:22:57 +0100

    created and sent token

--- controllers/authController.js
+++ controllers/authController.js
@@ -27,6 +27,37 @@ const login = asyncHandler(async (req, res, next) => {
       message: "Unauthorized",
     });
   }
+
+  const accessToken = jwt.sign(
+    {
+      UserInfo: {
+        username: foundUser.username,
+        roles: foundUser.roles,
+      },
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: "10s" }
+  );
+
+  const refreshToken = jwt.sign(
+    {
+      username: foundUser.username,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: "1d" }
+  );
+
+  // Create and secure cookie with refresh token
+  res.cookie("jwt", refreshToken, {
+    httpOnly: true, // accessible only by web server
+    secure: true, // only accessible over https
+    sameSite: "None", // cross-site request forgery
+    maxAge: 7 * 24 * 60 * 60 * 1000, // cookie expirey set to match refresh token
+  });
+
+  res.json({
+    accessToken,
+  });
 });
 
 const refresh = asyncHandler(async (req, res, next) => {});
