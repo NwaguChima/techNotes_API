3fbfca121bb62973f7434599835a3c00813a0fcd
NwaguChima <nwaguchima48@gmail.com>
2022-09-11 02:56:27 +0100

    ready for deployment

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://www.dandrepairshop.com",
-  "https://dandrepairshop.com",
+  "https://www.technotes-cn.onrender.com",
+  "https://technotes-cn.onrender.com",
   "http://192.168.0.140:3000",
 ];
 
